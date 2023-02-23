bd1a4159bb518f98c34f4bb737c1cb6227802257
Robinson <robinson@dorkbox.com>
2023-02-23 20:15:03 +0100

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ object Extras {
     // set for the project
     const val description = "Storage system for Java"
     const val group = "com.dorkbox"
-    const val version = "1.3"
+    const val version = "1.4"
 
     // set as project.ext
     const val name = "Storage"
--- src/dorkbox/storage/Storage.kt
+++ src/dorkbox/storage/Storage.kt
@@ -33,7 +33,7 @@ abstract class Storage(val logger: KLogger) : AutoCloseable {
         /**
          * Gets the version number.
          */
-        const val version = "1.3"
+        const val version = "1.4"
 
         init {
             // Add this project to the updates system, which verifies this class + UUID + version information
